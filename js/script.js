d11765a5e5187f4386692bfe5059c8534930db20
Michelle- <michukimichelle@gmail.com>
2020-03-03 09:09:43 +0300

    changes made to js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -7,3 +6,27 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quantity").value;
+    return parseInt(selectedQuantity);
+}
+function getTotalAmount() {
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
+    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    prompt("enter your location")
+    alert("You delivery fee is 150/=")
+    alert("You order will be delivered shortly")
+    alert("Continue shoppping with us each and every time .")
+}
